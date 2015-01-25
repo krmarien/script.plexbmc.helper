7f6d9bc1960d395f97107be6851de03347b30148
Kristof Mariën <krmarien@gmail.com>
2015-01-25 11:31:43 +0100

    Fixed an issue with name not send to Plex Server

--- resources/lib/settings.py
+++ resources/lib/settings.py
@@ -1,28 +1,22 @@
+import json
 import uuid
 import xbmc
 import xbmcaddon
-from xml.dom.minidom import parse
 
 settings = {}
-try:
-    guidoc = parse(xbmc.translatePath('special://userdata/guisettings.xml'))
-except:
-    print "Unable to read XBMC's guisettings.xml"    
 
 def getGUI(name):
-    global guidoc
-    if guidoc is None:
-        return False
-    return guidoc.getElementsByTagName(name)[0].nodeValue
-    return False
+    response = xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Settings.GetSettingValue", "params": {"setting": "' + name + '" }, "id": 1 }')
+    return json.loads(response)['result']['value']
+
+print getGUI('services.devicename')
 
 addon = xbmcaddon.Addon()
 plexbmc = xbmcaddon.Addon('plugin.video.plexbmc')
-
 settings['debug'] = addon.getSetting('debug') == "true"
 settings['gdm_debug'] = int(addon.getSetting('gdm_debug'))
-if addon.getSetting('use_xbmc_name'):
-    settings['client_name'] = getGUI('devicename')
+if addon.getSetting('use_xbmc_name') == "true":
+    settings['client_name'] = getGUI('services.devicename')
 else:
     settings['client_name'] = addon.getSetting('c_name')
 settings['uuid'] = str(addon.getSetting('uuid')) or str(uuid.uuid4())
